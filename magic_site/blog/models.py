22ca1e6b0d7d9fe01774cbf50ea1f2568497593f
Doug McClellan <DJMcClellan1966@gmail.com>
2020-03-04 23:52:58 -0500

    added blog app, updated main page

---
+++ magic_site/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.DateField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]
+
+    def __str__(self):
+        return self.title
